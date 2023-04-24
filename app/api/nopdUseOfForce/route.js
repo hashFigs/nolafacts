45ae0a55645dc87a61b121f84c31548e2d5d24ab
jordimintknight <93216318+jordimintknight@users.noreply.github.com>
2023-04-24 17:50:05 -0500

    compiling

--- app/api/nopdUseOfForce/route.js
+++ app/api/nopdUseOfForce/route.js
@@ -1,20 +1,15 @@
 export async function GET(request) {
     
     var options = {
-        method: "GET",
-                data: {
-            "$limit" : 5000,
-        //    "$$app_token" : "YOURAPPTOKENHERE"
-          }
+            method: "GET",
+            data: {
+                "$limit" : 5000,
+                 }
     }
 
     const res = await fetch(`https://data.nola.gov/resource/9mnw-mbde.json?$limit=10000`, options);
     
     return res;
 
-   // return new Response('this is the LA data')
-
-
-  //  
   }
   
